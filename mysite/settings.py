279a0e72004e122d377315f37c823931122a8e95
Stanley Ndagi <ndagis@gmail.com>
2024-10-06 11:49:53 +0300

    🔧 config(mysite/settings.py): update settings >>> ⏰ <10m
    ALLOWED_HOSTS for local development and deployment to pythonanywhere and via c9
    TIME_ZONE to match author's timezone
    add STATIC_ROOT

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hfbgpabz@@03jetub3m#j3jt^%owo-wzslm1&(ky=&ju(t!twz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com', '.amazonaws.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
